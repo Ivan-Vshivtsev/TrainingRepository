de189360b640a2095423802f61e950ab42269045
Ivan Vshivtsev <ivan.vshivtsev@playrix.com>
2023-11-27 16:48:36 +0600

    add file

---
+++ 0/!DownloadExact.cmd
@@ -0,0 +1,10 @@
+@echo off
+
+set RUN_FROM_SCRIPT=true
+
+echo Run last successful build from current branch
+call "Main\Download.cmd" --tc-use-gui-auth --category exact
+
+set PREPARE_RESULT=%ERRORLEVEL%
+if %PREPARE_RESULT% neq 0 pause
+exit /b %PREPARE_RESULT%
